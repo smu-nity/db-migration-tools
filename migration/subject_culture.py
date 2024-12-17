32c845305005501cea8a4a7299f573262d08d909
Hyunmin Choi <hyunmin-choi@naver.com>
2024-12-17 20:25:15 +0900

    [FEATURE] SUBJECT_CULTURE 테이블 마이그레이션 (#6)
    * delete: .idea 폴더 (#1)
    
    * chore: .gitignore 수정 (#1)
    
    * chore: 깃허브 이슈 템플릿 수정 (#1)
    
    * feat: 교양과목 데이터셋 추가 (#1)
    
    * feat: 교양과목 마이그레이션 스크립트 (#1)
    
    * refactor: 교양과목 마이그레이션 스크립트 (#1)
    
    * refactor: 경로 수정 (#1)
    
    * refactor: 코드 리팩토링 (#1)

---
+++ migration/subject_culture.py
@@ -0,0 +1,37 @@
+import json
+import os
+
+
+def read_json(file):
+    with open(file, 'r') as file:
+        return json.load(file)
+
+
+def save_file(text, file_path):
+    with open(file_path, 'w') as file:
+        file.write(text)
+
+
+def culture(path, domain):
+    sql = ''
+    sub_domain = domain.split('.')[0]
+    datas = read_json(os.path.join(path, domain))
+    for data in datas:
+        name, number, credit = data['name'], data['number'], data['credit']
+        sql += f"('{sub_domain}', {credit}, '{name}', '{number}'),\n\t   "
+    return sql
+
+
+def main():
+    sql = 'INSERT INTO subject_culture (sub_domain, credit, name, number)\nVALUES '
+    dataset_path, result_file = './datasets/subject_culture', './sql/subject_culture.sql'
+    domains = os.listdir(dataset_path)
+    for domain in domains:
+        sql += culture(dataset_path, domain)
+    sql = sql[:-6]
+    sql += ';\n\nCOMMIT;\n'
+    save_file(sql, result_file)
+
+
+if __name__ == "__main__":
+    main()
