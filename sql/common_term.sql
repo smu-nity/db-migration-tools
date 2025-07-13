978c579b1f29c542dc5b4845ca4533b51a0478b5
Hyunmin Choi <hyunmin-choi@naver.com>
2025-07-13 18:55:27 +0900

    [FEATURE] 2025학년도 2학기 교육과정 업데이트 (#33)
    * chore: 전공 과목 업데이트 (#32)
    
    * chore: 교양 과목 업데이트 (#32)
    
    * chore: 학기 업데이트 (#32)

--- sql/common_term.sql
+++ sql/common_term.sql
@@ -3,6 +3,7 @@ VALUES (2023, 'FIRST'),
        (2023, 'SECOND'),
        (2024, 'FIRST'),
        (2024, 'SECOND'),
-       (2025, 'FIRST');
+       (2025, 'FIRST'),
+       (2025, 'SECOND');
 
 COMMIT;
